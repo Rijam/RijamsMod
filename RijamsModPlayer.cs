f16000672dd96073c50275c67ae7b55dd4210311
Rijam <jacobrmoreno@hotmail.com>
2021-10-17 00:43:37 -0600

    Add files via upload

---
+++ RijamsModPlayer.cs
@@ -0,0 +1,96 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+
+namespace RijamsMod
+{
+	public class RijamsModPlayer : ModPlayer
+	{
+		public bool cutestIceFairy;
+		public bool acidicSpiderling;
+		public bool guideToProperFlightTechniques;
+		public bool summonersGlove;
+		public bool daybreakStone;
+		public bool breathingPack;
+		public int breathingPackTimer;
+		//public bool breathingPackUsed = true;
+
+		public override void ResetEffects()
+		{
+			cutestIceFairy = false;
+			acidicSpiderling = false;
+			guideToProperFlightTechniques = false;
+			summonersGlove = false;
+			daybreakStone = false;
+			breathingPack = false;
+			//breathingPackUsed = true;
+			//breathingPackTimer = 0;
+		}
+		public override void clientClone(ModPlayer clientClone)
+		{
+			RijamsModPlayer clone = clientClone as RijamsModPlayer;
+		}
+		public override void SyncPlayer(int toWho, int fromWho, bool newPlayer)
+		{
+			ModPacket packet = mod.GetPacket();
+			packet.Write((byte)player.whoAmI);
+			packet.Send(toWho, fromWho);
+		}
+		public override void SendClientChanges(ModPlayer clientPlayer)
+        {
+			RijamsModPlayer clone = clientPlayer as RijamsModPlayer;
+		}
+		public override void PostUpdateEquips()
+        {
+			if (guideToProperFlightTechniques)
+            {
+				if (player.wingTimeMax > 0)
+				{
+					player.wingTimeMax += 60;
+					player.jumpSpeedBoost += 2f;
+					player.moveSpeed += 2;
+				}
+			}
+			if (breathingPack)
+            {
+				//Main.NewText("breathingPackUsed " + breathingPackUsed);
+				//Main.NewText("breathingPackTimer " + breathingPackTimer);
+				//Main.NewText("player.wet " + player.wet);
+				if (player.breath <= 1 )//&& breathingPackUsed == false)
+				{
+					//breathingPackUsed = true;
+					//player.GetModPlayer<RijamsModPlayer>().breathingPackUsed = true;
+					breathingPackTimer++;
+					if (breathingPackTimer == 1 && !Main.dedServ)
+                    {
+						Main.PlaySound(SoundLoader.customSoundType, (int)player.Center.X, (int)player.Center.Y, mod.GetSoundSlot(SoundType.Custom, "Sounds/Custom/beep"), 0.5f, 1.5f);
+					}
+					if (breathingPackTimer == 60 && !Main.dedServ)
+					{
+						Main.PlaySound(SoundLoader.customSoundType, (int)player.Center.X, (int)player.Center.Y, mod.GetSoundSlot(SoundType.Custom, "Sounds/Custom/beep"), 0.5f, 2f);
+					}
+					if (breathingPackTimer >= 120)
+					{
+						if (!Main.dedServ)
+                        {
+							Main.PlaySound(SoundLoader.customSoundType, (int)player.Center.X, (int)player.Center.Y, mod.GetSoundSlot(SoundType.Custom, "Sounds/Custom/beep"), 0.5f, 0.5f);
+						}
+						breathingPackTimer = 0;
+						//breathingPackUsed = true;
+						//player.GetModPlayer<RijamsModPlayer>().breathingPackUsed = true;
+						player.breath += player.breathMax;
+					}
+				}
+				/*if (player.wet == false || player.honeyWet == false || player.lavaWet == false)
+                {
+					breathingPackTimer = 0;
+					//breathingPackUsed = false;
+					//player.GetModPlayer<RijamsModPlayer>().breathingPackUsed = false;
+				}*/
+            }
+        }
+	}
+}
---
+++ RijamsModWorld.cs
@@ -0,0 +1,196 @@
+using RijamsMod.Items;
+using RijamsMod.NPCs;
+//using RijamsMod.Tiles;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using System.IO;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent.Generation;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+using Terraria.World.Generation;
+
+namespace RijamsMod
+{ 
+    public class RijamsModWorld : ModWorld
+    {
+		public static bool savedHarpy = false;
+        public static bool intTravArived = false;
+        public static bool intTravQuestOddDevice = false;
+        public static bool intTravQuestBlankDisplay = false;
+        public static bool intTravQuestTPCore = false;
+        public static bool intTravQuestRyeJam = false;
+        public static bool intTravQuestMagicOxygenizer = false;
+
+        public override void Initialize()
+        {
+			savedHarpy = false;
+            intTravArived = false;
+            intTravQuestOddDevice = false;
+            intTravQuestBlankDisplay = false;
+            intTravQuestTPCore = false;
+            intTravQuestRyeJam = false;
+            intTravQuestMagicOxygenizer = false;
+        }
+
+        public override TagCompound Save()
+        {
+            var downed = new List<string>();
+            //var rescueNPCs = new List<string>();
+            //var intTravQuest = new List<string>();
+            if (savedHarpy)
+            {
+                downed.Add("savedHarpy");
+            }
+            if (intTravArived)
+            {
+                downed.Add("intTravArived");
+            }
+            if (intTravQuestOddDevice)
+            {
+                downed.Add("intTravQuestOddDevice");
+            }
+            if (intTravQuestBlankDisplay)
+            {
+                downed.Add("intTravQuestBlankDisplay");
+            }
+            if (intTravQuestTPCore)
+            {
+                downed.Add("intTravQuestTPCore");
+            }
+            if (intTravQuestRyeJam)
+            {
+                downed.Add("intTravQuestRyeJam");
+            }
+            if (intTravQuestMagicOxygenizer)
+            {
+                downed.Add("intTravQuestBreathingPack");
+            }
+
+
+            return new TagCompound
+            {
+
+                ["downed"] = downed
+                //["rescueNPCs"] = rescueNPCs
+                //["intTravQuest"] = intTravQuest
+            };
+
+            /*TagCompound tag = new TagCompound();
+            tag["savedHarpy"] = savedHarpy;
+            tag["intTravArived"] = intTravArived;
+            tag["intTravQuestOddDevice"] = intTravQuestOddDevice;
+            tag["intTravQuestBlankDisplay"] = intTravQuestBlankDisplay;
+
+            return tag;*/
+        }
+        public override void Load(TagCompound tag)
+        {
+            var downed = tag.GetList<string>("downed");
+            //var rescueNPCs = tag.GetList<string>("rescueNPCs");
+            //var intTravQuest = tag.GetList<string>("intTravQuest");
+            savedHarpy = downed.Contains("savedHarpy");
+            intTravArived = downed.Contains("intTravArived");
+            intTravQuestOddDevice = downed.Contains("intTravQuestOddDevice");
+            intTravQuestBlankDisplay = downed.Contains("intTravQuestBlankDisplay");
+            intTravQuestTPCore = downed.Contains("intTravQuestTPCore");
+            intTravQuestRyeJam = downed.Contains("intTravQuestRyeJam");
+            intTravQuestMagicOxygenizer = downed.Contains("intTravQuestBreathingPack");
+
+            /*savedHarpy = tag.GetBool("savedHarpy");
+            intTravArived = tag.GetBool("intTravArived");
+            intTravQuestOddDevice = tag.GetBool("intTravQuestOddDevice");
+            intTravQuestBlankDisplay = tag.GetBool("intTravQuestBlankDisplay");*/
+        }
+        public override void LoadLegacy(BinaryReader reader)
+        {
+            int loadVersion = reader.ReadInt32();
+            if (loadVersion == 0)
+            {
+                BitsByte flags = reader.ReadByte();
+                savedHarpy = flags[0];
+                intTravArived = flags[1];
+                intTravQuestOddDevice = flags[2];
+                intTravQuestBlankDisplay = flags[3];
+                intTravQuestTPCore = flags[4];
+                intTravQuestRyeJam = flags[5];
+                intTravQuestMagicOxygenizer = flags[6];
+            }
+            else
+            {
+                mod.Logger.WarnFormat("RijamsMod: Unknown loadVersion: {0}", loadVersion);
+            }
+        }
+
+        public override void NetSend(BinaryWriter writer)
+        {
+            //Remember that Bytes/BitsByte only have 8 entries. If you have more than 8 flags you want to sync, use multiple BitsByte
+            var flags = new BitsByte();
+            flags[0] = savedHarpy;
+            flags[1] = intTravArived;
+            flags[2] = intTravQuestOddDevice;
+            flags[3] = intTravQuestBlankDisplay;
+            flags[4] = intTravQuestTPCore;
+            flags[5] = intTravQuestRyeJam;
+            flags[6] = intTravQuestMagicOxygenizer;
+            writer.Write(flags);
+
+            /*writer.Write(savedHarpy);
+            writer.Write(intTravArived);*/
+            //writer.Write(intTravQuestOddDevice);
+            //writer.Write(intTravQuestBlankDisplay);
+        }
+        public override void NetReceive(BinaryReader reader)
+        {
+            BitsByte flags = reader.ReadByte();
+            savedHarpy = flags[0];
+            intTravArived = flags[1];
+            intTravQuestOddDevice = flags[2];
+            intTravQuestBlankDisplay = flags[3];
+            intTravQuestTPCore = flags[4];
+            intTravQuestRyeJam = flags[5];
+            intTravQuestMagicOxygenizer = flags[6];
+
+            /*savedHarpy = reader.ReadBoolean();
+            intTravArived = reader.ReadBoolean();*/
+            //intTravQuestOddDevice = reader.ReadBoolean();
+            //intTravQuestBlankDisplay = reader.ReadBoolean();
+        }
+
+        public static void UpdateWorldBool() //from Calamity's Vanities
+        {
+            if (Main.netMode == NetmodeID.Server)
+            {
+                NetMessage.SendData(MessageID.WorldData);
+            }
+        }
+        /*public static void TransformHarpy()
+        {
+            savedHarpy = true;
+            UpdateWorldBool();
+        }*/
+        public static void SetIntTravArived()
+        {
+            intTravArived = true;
+            UpdateWorldBool();
+        }
+        public static void SetIntTravQuestOddDevice()
+        {
+            intTravQuestOddDevice = true;
+
+            UpdateWorldBool();
+        }
+        public static void SetIntTravQuestBlankDisplay()
+        {
+            intTravQuestBlankDisplay = true;
+            //ModPacket packet = mod.GetPacket();
+            //packet.Write((byte)RijamsModMessageType.SetQuestBlankDisplay);
+            //packet.Send();
+            UpdateWorldBool();
+        }
+    }
+}
